627a0160927574d6c239f2cb60770f064a0b71c9
Chrystian R. Campos <chrystianrcampos@gmail.com>
2025-05-02 21:18:52 -0300

    fix: changing output vm public ip that access db through ssh tunnel

--- outputs.tf
+++ outputs.tf
@@ -7,5 +7,5 @@ output "vm2_ssh_access" {
 }
 
 output "mysql_db_access" {
-  value = "ssh -i ${var.ssh_private_key} -L 3306:${oci_mysql_mysql_db_system.database.endpoints[0].ip_address}:3306 ubuntu@${oci_core_public_ip.vm2_public_ip.ip_address}"
+  value = "ssh -i ${var.ssh_private_key} -L 3306:${oci_mysql_mysql_db_system.database.endpoints[0].ip_address}:3306 ubuntu@${oci_core_public_ip.vm_public_ip.ip_address}"
 }
